7ee3d387427f8737ca963562c221f5274e835eda
Yung <yung.tk17@gmail.com>
2015-11-08 23:52:47 +0800

    first release with azure active directory authentication

---
+++ resources/views/index.blade.php
@@ -0,0 +1,17 @@
+<script src="https://cdn.auth0.com/js/lock-7.9.min.js"></script>
+<script type="text/javascript">
+  
+  var lock = new Auth0Lock('DyxDQuLCT7Ul4tLPgOMvptTDszBKNyDz', 'yungtk17.auth0.com');
+  
+  
+  function signin() {
+    lock.show({
+        callbackURL: 'http://localhost:8000/'
+      , responseType: 'code'
+      , authParams: {
+        scope: 'openid profile'
+      }
+    });
+  }
+</script>
+<button onclick="window.signin();">Login</button>
